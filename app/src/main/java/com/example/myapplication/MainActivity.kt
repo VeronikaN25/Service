96b562781b3dbc418ea8968267061e554d0b7636
Veeя <novichikhina.05@mail.ru>
2024-03-16 18:16:27 +0600

    Finish foreground

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,18 +1,46 @@
 package com.example.myapplication
 
 import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import androidx.core.content.ContextCompat
+import com.example.myapplication.databinding.ActivityMainBinding
 
+const val TAG="TESTTEST"
 
 class MainActivity : AppCompatActivity() {
 
+    private var binding:ActivityMainBinding?=null
+//    private var receiver=NetworkChangeReceiver()
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "Отработал onCreate")
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+
+//        val intentFilter=IntentFilter("androind.net.conn.CONNECTIVITY_CHANNEL")
+//        registerReceiver(receiver,intentFilter)
+
+        val intentForeground=Intent(this,MyForegroundService::class.java)
+
+        binding?.let{
+            it.btnForegroundStart.setOnClickListener {
+                ContextCompat.startForegroundService(this, intentForeground)
+            }
+
+            it.btnForegroundStop.setOnClickListener {
+                stopService(intentForeground)
+            }
+        }
     }
-    
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
 
 
 }
--- app/src/main/java/com/example/myapplication/MyForegroundService.kt
+++ app/src/main/java/com/example/myapplication/MyForegroundService.kt
@@ -1,3 +1,76 @@
 package com.example.myapplication
 
-class MyForegroundService {}
+import android.annotation.SuppressLint
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.IBinder
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+private const val NOTIFICATION_ID=123
+private const val NOTIFICATION_CHANNEL_ID="foreground_service"
+class MyForegroundService: Service() {
+
+    private lateinit var notification: Notification
+
+    private val notificationManager by lazy {
+        getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+    }
+
+    @SuppressLint("ForegroundServiceType")
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
+        notification = createNotification("Start service")
+
+        CoroutineScope(Dispatchers.IO)
+            .launch {
+                Log.d("TESTTEST","Start coroutine")
+                for(item in 10 downTo 0){
+                    Log.d("TESTTEST","Start for $item")
+                    val updateNotification=createNotification(textMessage =" $item")
+                    notificationManager.notify(NOTIFICATION_ID,updateNotification)
+                    delay(1000L)
+                }
+                stopSelf()
+            }
+
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    private fun createNotification(textMessage:String):Notification{
+
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            val channel=NotificationChannel(
+                NOTIFICATION_CHANNEL_ID,
+                NOTIFICATION_CHANNEL_ID,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        return NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+            .setSmallIcon(R.drawable.notif)
+            .setContentTitle("Timer")
+            .setContentText(textMessage)
+            .setOngoing(true)
+            .setOnlyAlertOnce(true)
+            .build()
+
+    }
+
+    //intent системный обьект  , который указывает намеривание которое мы хотим выполнить
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+
+}
